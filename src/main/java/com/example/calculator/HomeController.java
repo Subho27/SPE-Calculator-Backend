a5f12f8b56b23e9f21da01fed1bdc5196e01608f
Subhodip Rudra <42699151+Subho27@users.noreply.github.com>
2024-02-16 02:23:12 +0530

    Update HomeController.java

--- src/main/java/com/example/calculator/HomeController.java
+++ src/main/java/com/example/calculator/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculator")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://16.171.181.71:8084")
 public class HomeController {
 
     @GetMapping("/square-root")
